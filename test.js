a24d080896ec524190719e11c28d4592ad0e489d
CloudyNo <noahagdam@gmail.com>
2021-04-26 13:48:07 +0200

    this still broken b whatever

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-function yes() {
-    alert("nut");
+var x = document.getElementById("popup").style.display = "none"
+function clicked() {
+    document.getElementById("popup").style.display = "block"
 }
