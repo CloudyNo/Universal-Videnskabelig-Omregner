4aabe0da5e6cc9c402e66d55af198b993227dc73
CloudyNo <noahagdam@gmail.com>
2021-04-26 11:15:06 +0200

    it fucking works

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-const Swal = reqire('sweetalert2')
-document.querySelector(".button").addEventListener('click', function(){
-    Swal.fire("Our First Alert");
-  });
+function yes() {
+    alert("nut");
+}
